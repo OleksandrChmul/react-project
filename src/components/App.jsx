1faaa76263926e3abd222743f369255fd48c67c7
OleksandrChmul <al.chmul@gmail.com>
2024-02-09 23:54:50 +0200

    upd2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-import { Profile } from './Profile'
+import { Profile } from './Profile/Profile'
 import userData from '../userData.json'
-import { FriendList } from './FriendList'
+import { FriendList } from './FriendList/FriendList'
 import friends from '../friends.json'
 import transactions from '../transactions.json'
-import { TransactionHistory } from './TransactionHistory'
+import { TransactionHistory } from './TransactionHistory/TransactionHistory'
 
 const App = () => {
 	return (
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,14 @@
+import clsx from 'clsx'
+import css from './FriendListItem.module.css'
+
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+	return (
+		<div className={css.item}>
+			<img className={css.avatar} src={avatar} alt='Avatar' width='48' />
+			<p className={css.name}>{name}</p>
+			<p className={clsx(css.status, isOnline ? css.online : css.offline)}>
+				{isOnline ? 'Online' : 'Offline'}
+			</p>
+		</div>
+	)
+}
