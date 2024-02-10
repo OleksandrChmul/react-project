aae3c0f4d80708c87433e3404007ea9744377be2
Oleksandr Chmul <al.chmul@gmail.com>
2024-02-10 14:05:52 +0200

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -5,16 +5,16 @@ export const TransactionHistory = ({ items }) => {
 		<table className={css.transactionHistory}>
 			<thead>
 				<tr className={css.tr}>
-					<th className={css.th}>Amount</th>
 					<th className={css.th}>Type</th>
+					<th className={css.th}>Amount</th>
 					<th className={css.th}>Currency</th>
 				</tr>
 			</thead>
 
 			<tbody>
-				{items.map((item, i) => {
+				{items.map(item => {
 					return (
-						<tr className={css.tr} key={i}>
+						<tr className={css.tr} key={item.id}>
 							<td className={css.td}>{item.type}</td>
 							<td className={css.td}>{item.amount}</td>
 							<td className={css.td}>{item.currency}</td>
